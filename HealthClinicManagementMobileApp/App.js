5bf05a00a43e66ecab9b779511ace07c0825dd0d
lokhue <2151013052loc@gmail.com>
2024-01-31 22:44:36 +0700

    mobile app first commit

---
+++ HealthClinicManagementMobileApp/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.t}>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  t: {
+    fontSize: 50,
+    color: 'blue',
+    fontWeight: 'bold',
+  }
+});
